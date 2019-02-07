9ffe8b51ac26cbbbbffb5d62d280ae1ddb7b6b76
ko <ko@MTES01-42PC>
2019-02-07 16:29:46 +0900

    chart test text read drow chart

--- chartTest/Form1.Designer.cs
+++ chartTest/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(793, 446);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.chart1_mousewheel);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.chart1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
-        }
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- chartTest/Form1.cs
+++ chartTest/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
 
 namespace chartTest
 {
@@ -15,6 +18,55 @@ namespace chartTest
         public Form1()
         {
             InitializeComponent();
+            
+            StreamReader objReader = new StreamReader("c:\\Random.txt");
+            string sLine = "";
+            ArrayList arrText = new ArrayList();
+
+            int i = 0;
+            while (sLine != null)
+            {
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                {
+                    arrText.Add(sLine);
+                    i = Int32.Parse(sLine);
+                }
+                chart1.Series[0].Points.AddXY(0, i);
+            }
+            objReader.Close();
+
+        }
+        
+        private void chart1_mousewheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(e.Delta);
+            if(e.Delta > 0)
+            {
+                //zoom
+                chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+
+                double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+
+                int posXStart = (int)(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 2);
+                int posXFinish = (int)(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.Y) + (xMax - xMin) / 2);
+
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+            }
+            else
+            {
+                //chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+
+                int posXStart = (int)(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) * 2);
+                int posXFinish = (int)(chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.Y) + (xMax - xMin) * 2);
+
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+            }
         }
     }
 }
